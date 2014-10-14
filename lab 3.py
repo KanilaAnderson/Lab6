ce9b0cbe9e8cb5d70140383ad40da7d97d498aae
KanilaAnderson <Kanilaanderson@gmail.com>
2014-10-14 13:38:11 -0500

    FIRST

---
+++ lab 3.py
@@ -0,0 +1,21 @@
+def isequaltotwo(num1): 
+    if num1 ==2:
+        print "Yes!" 
+    else:
+        print "no!"
+        print "end of fuction" 
+        
+def comparetoten (num1):
+    if num1 <=10:
+        print"less than or equal to 10!"
+    else:
+         print"not less than or equal to 10!"
+def comparetotenagain(num1):
+    if num1>10:
+        print"greater than 10!"
+    if num1<10:
+        print "less than 10!" 
+    if num1 ==10:
+        print "10 exactly!"
+         
+ 
